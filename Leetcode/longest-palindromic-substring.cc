d3378d82fba45659312533c6e1db710af983db52
alexchen <alexchen@alexchendeMacBook-Air.local>
2019-01-06 19:21:27 +0800

    leetcode:5 最长回文子串

---
+++ Leetcode/longest-palindromic-substring.cc
@@ -0,0 +1,78 @@
+/*
+    给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+    示例 1：
+        输入: "babad"
+        输出: "bab"
+        注意: "aba" 也是一个有效答案。
+    示例 2：
+        输入: "cbbd"
+        输出: "bb"
+*/
+
+#include <cassert>
+#include <iostream>
+#include <set>
+#include <string>
+
+using namespace std;
+
+class Solution {
+  public:
+    string longestPalindrome(string s) {}
+};
+
+string stringToString(string input) {
+    assert(input.length() >= 2);
+    string result;
+    for (int i = 1; i < input.length() - 1; i++) {
+        char currentChar = input[i];
+        if (input[i] == '\\') {
+            char nextChar = input[i + 1];
+            switch (nextChar) {
+                case '\"':
+                    result.push_back('\"');
+                    break;
+                case '/':
+                    result.push_back('/');
+                    break;
+                case '\\':
+                    result.push_back('\\');
+                    break;
+                case 'b':
+                    result.push_back('\b');
+                    break;
+                case 'f':
+                    result.push_back('\f');
+                    break;
+                case 'r':
+                    result.push_back('\r');
+                    break;
+                case 'n':
+                    result.push_back('\n');
+                    break;
+                case 't':
+                    result.push_back('\t');
+                    break;
+                default:
+                    break;
+            }
+            i++;
+        } else {
+            result.push_back(currentChar);
+        }
+    }
+    return result;
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        string s = stringToString(line);
+
+        string ret = Solution().longestPalindrome(s);
+
+        string out = (ret);
+        cout << out << endl;
+    }
+    return 0;
+}

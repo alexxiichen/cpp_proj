31a9502560920b048087a87a2527714384ceaef6
alexxiichen <alexxiichen@gmail.com>
2019-04-07 03:48:55 +0800

    some change

---
+++ 3rd/autobuild.sh
@@ -0,0 +1,49 @@
+# /bin/bash
+cd googletest
+mkdir build_dir
+cd build_dir
+cmake ..
+make -j 24
+cd ../..
+
+cd grpc
+mkdir build_dir
+cd build_dir
+cmake ..
+make -j 24
+cd ../..
+
+cd leveldb
+mkdir build_dir
+cd build_dir
+cmake ..
+make -j 24
+cd ../..
+
+cd libco
+mkdir build_dir
+cd build_dir
+cmake ..
+make -j 24
+cd ../..
+
+cd protobuf
+mkdir build_dir
+cd build_dir
+cmake ..
+make -j 24
+cd -
+
+cd snappy
+mkdir build_dir
+cd build_dir
+cmake ..
+make -j 24
+cd ../..
+
+cd rocksdb
+mkdir build_dir
+cd build_dir
+cmake ..
+make -j 24
+cd ../..

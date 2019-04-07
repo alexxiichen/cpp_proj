320cec4af6d01902fa7b03afd659416477f865e5
alexxiichen <alexxiichen@gmail.com>
2019-04-07 22:35:23 +0800

    modify 3rd/README.md

--- 3rd/README.md
+++ 3rd/README.md
@@ -8,3 +8,7 @@
 - [snappy](https://github.com/google/snappy)
 - [libco](https://github.com/tencent/libco)
 - [openssl](https://github.com/openssl/openssl)
+- [FlamGraph](https://github.com/brendangregg/FlameGraph)
+- [gperftools](https://github.com/gperftools/gperftools)
+- [pprof](https://github.com/google/pprof)
+- [graphviz](https://gitlab.com/graphviz/graphviz)
--- 3rd/autobuild.sh
+++ 3rd/autobuild.sh
@@ -1,49 +1,69 @@
 # /bin/bash
+workdir=$(cd $(dirname $0); pwd)
+
 cd googletest
 mkdir build_dir
 cd build_dir
 cmake ..
 make -j 24
-cd -
+cd $workdir
 
 cd grpc
 mkdir build_dir
 cd build_dir
 cmake ..
 make -j 24
-cd -
+cd $workdir
 
 cd leveldb
 mkdir build_dir
 cd build_dir
 cmake ..
 make -j 24
-cd -
+cd $workdir
 
 cd libco
 mkdir build_dir
 cd build_dir
 cmake ..
 make -j 24
-cd -
+cd $workdir
 
 cd protobuf/cmake
 mkdir build_dir
 cd build_dir
 cmake ..
 make -j 24
-cd -/..
+make check
+make install
+cd $workdir
 
 cd snappy
 mkdir build_dir
 cd build_dir
 cmake ..
 make -j 24
-cd -
+cd $workdir
 
 cd rocksdb
 mkdir build_dir
 cd build_dir
 cmake ..
 make -j 24
-cd -
+cd $workdir
+
+cd openssl
+./Configure
+./config
+make -j 24
+make check
+make install
+cd $workdir
+
+cd gperftools
+./configure
+make -j 24
+make check
+make install
+cd $workdir
+
